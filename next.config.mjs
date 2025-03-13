34d26c3f9897cbc8683b29910f591dad046df9de
Abhimanyu kaushik <75792535+AbhimanyuKaushik@users.noreply.github.com>
2025-03-13 22:18:38 +0530

    revamped

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains:['search.brave.com','localhost','images.indianexpress.com','images.unsplash.com','images.pexels.com','www.shutterstock.com','caravan-frontend-d6jzk49qr-abhimanyu-kaushiks-projects.vercel.app','caravanbackend-c95x.onrender.com']
+        domains:['search.brave.com','localhost','images.indianexpress.com','images.unsplash.com','images.pexels.com','www.shutterstock.com','https://caravanbackend-c95x.onrender.com']
     }
 };
 
